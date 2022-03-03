2f702680ce4c9036838987f7d0bb9d79646f5198
maxgarr <maxime.garron17@gmail.com>
2022-03-03 15:55:55 +0100

    Update table clients

--- src/view/user/gestion_commerciale/clients.php
+++ src/view/user/gestion_commerciale/clients.php
@@ -1,32 +1,33 @@
-<h1 class="text-center my-5">Affichage clients</h1>
+<h1 class="text-center my-5">Liste des clients</h1>
 
-<table id="test" class="display">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>ID Secteur</th>
-            <th>Raison sociale</th>
-            <th>Code postal</th>
-            <th>Ville</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($clients as $client) : ?>        
+
+<div class="m-5">
+    <table id="test" class="display text-center">
+        <thead>
             <tr>
-                <td><?= htmlspecialchars($client->getId_client()) ?></td>
-                <td><?= htmlspecialchars($client->getId_sect()) ?></td>
-                <td><?= htmlspecialchars($client->getRaison_sociale()) ?></td>
-                <td><?= htmlspecialchars($client->getCode_postal_client()) ?></td>
-                <td><?= htmlspecialchars($client->getVille_client()) ?></td>
-                <td>
-                    <form action="<?="clientProfil=" . htmlspecialchars($client->getId_client())?>" method="get">
-                        <button type="submit" class="button btn btn-secondary">></a>
-                    </form>
-                </td>
+                <th>ID</th>
+                <th>ID Secteur</th>
+                <th>Raison sociale</th>
+                <th>Code postal</th>
+                <th>Ville</th>
+                <th>Afficher</th>
             </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
-
-
-
+        </thead>
+        <tbody>
+            <?php foreach ($clients as $client) : ?>
+                <tr>
+                    <td><?= htmlspecialchars($client->getId_client()) ?></td>
+                    <td><?= htmlspecialchars($client->getId_sect()) ?></td>
+                    <td><?= htmlspecialchars($client->getRaison_sociale()) ?></td>
+                    <td><?= htmlspecialchars($client->getCode_postal_client()) ?></td>
+                    <td><?= htmlspecialchars($client->getVille_client()) ?></td>
+                    <td>
+                        <form action="<?= "clientProfil=" . htmlspecialchars($client->getId_client()) ?>" method="get">
+                            <button type="submit" class="button btn btn-secondary">></a>
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
