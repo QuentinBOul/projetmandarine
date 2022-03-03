9400f329246280c336974dad7db1e5cdcc264a21
maxgarr <maxime.garron17@gmail.com>
2022-03-03 11:55:15 +0100

    update

--- src/view/user/gestion_commerciale/clients.php
+++ src/view/user/gestion_commerciale/clients.php
@@ -1,2 +1,52 @@
 <h1 class="text-center my-5">Affichage clients</h1>
 
+<table id="test" class="display">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>ID Secteur</th>
+            <th>Raison sociale</th>
+            <th>Code postal</th>
+            <th>Ville</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 2</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 2</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 2</td>
+        </tr>
+            <td>4</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 2</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 2</td>
+        </tr>
+    </tbody>
+</table>
+
+
+
