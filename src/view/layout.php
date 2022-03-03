f666e77da79963a7aa6c1ede3d74b83c6b3998ba
Kevin BRANTONE <kbrantone@gmail.com>
2022-03-03 10:57:16 +0100

    chemin js et css

--- src/view/layout.php
+++ src/view/layout.php
@@ -6,12 +6,16 @@
     <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" defer></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"
+        defer></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js" defer></script>
-    <link rel="stylesheet" href="../../public/assets/css/style.css">
-    <script src="../../public/assets/js/main.js" defer></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js" defer>
+    </script>
+    <link rel="stylesheet" href="./public/assets/css/style.css">
+    <script src="./public/assets/js/main.js" defer></script>
     <title>Projet Mandarine</title>
 
 </head>
@@ -57,7 +61,8 @@
 
     <?= $content ?>
 
-    <footer class="text-center" style="width: 100%; height: 100px; background-color: gray; position: absolute; bottom: 0;">
+    <footer class="text-center"
+        style="width: 100%; height: 100px; background-color: gray; position: absolute; bottom: 0;">
         <h1>footer</h1>
     </footer>
 
