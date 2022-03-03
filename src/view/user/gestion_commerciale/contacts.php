74e7c57f70bf80f6f582912fa3a12c9328b32137
maxgarr <maxime.garron17@gmail.com>
2022-03-03 16:23:55 +0100

    add page contacts + contactProfil

--- src/view/user/gestion_commerciale/contacts.php
+++ src/view/user/gestion_commerciale/contacts.php
@@ -1,2 +1,32 @@
-<h1 class="text-center my-5">Affichage contacts</h1>
+<h1 class="text-center my-5">Liste des contacts</h1>
 
+<div class="m-5">
+    <table id="test" class="display text-center">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>ID Client</th>
+                <th>ID Fonction</th>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Afficher</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($contacts as $contact) : ?>
+                <tr>
+                    <td><?= htmlspecialchars($contact->getId_contact()) ?></td>
+                    <td><?= htmlspecialchars($contact->getId_client()) ?></td>
+                    <td><?= htmlspecialchars($contact->getId_fonc()) ?></td>
+                    <td><?= htmlspecialchars($contact->getNom_contact()) ?></td>
+                    <td><?= htmlspecialchars($contact->getPrenom_contact()) ?></td>
+                    <td>
+                        <form action="<?= "contactProfil=" . htmlspecialchars($contact->getId_contact()) ?>" method="get">
+                            <button type="submit" class="button btn btn-secondary">></a>
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
