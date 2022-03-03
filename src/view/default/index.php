7908f777b83f848c85e350b50d79eb9120ce8d1e
QuentinBOul <100684130+QuentinBOul@users.noreply.github.com>
2022-03-03 18:45:56 +0100

    modif default/index.php
    Mise en place temporaire du message d'erreur de connexion

--- src/view/default/index.php
+++ src/view/default/index.php
@@ -1,1 +1,9 @@
+<div class="container">
+<?php if (isset($error) && !empty($error)) : ?>
+    <div class="alert alert-danger" role="alert">
+        <?= $error ?>
+    </div>
+    <?php endif; ?>
+</div>
+
 <h1>PAGE ACCUEIL</h1>
