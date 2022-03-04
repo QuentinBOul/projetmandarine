ccc3a18a90cf747f7163e4149a190fa3fb09e376
maxgarr <maxime.garron17@gmail.com>
2022-03-04 17:43:20 +0100

    Update dashboard commercial

--- src/view/user/gestion_commerciale/dashboard.php
+++ src/view/user/gestion_commerciale/dashboard.php
@@ -1,6 +1,22 @@
 <h1 class="text-center my-5">DASHBOARD Gestion commerciale</h1>
 
-<div class="container" style="width: 300px; margin-left:auto; margin-right:auto;">
-    <a href="clients" class="btn btn-secondary" role="button">Clients</a>
-    <a href="contacts" class="btn btn-secondary" role="button">Contacts</a>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-3 offset-sm-3">
+            <div class="card">
+                <div class="card-body text-center" style="background-color: rgba(112, 114, 110, 0.5); box-shadow: -10px 5px 10px rgba(112, 114, 110, 0.8);">
+                    <h5 class="card-title">Liste des Clients</h5>
+                    <a href="clients" class="btn btn-secondary" role="button">Afficher</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-body text-center" style="background-color: rgba(112, 114, 110, 0.5); box-shadow: 10px 5px 10px rgba(112, 114, 110, 0.8);">
+                    <h5 class="card-title">Liste des Contacts</h5>
+                    <a href="contacts" class="btn btn-secondary" role="button">Afficher</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
