a468b58343103faf18d77d735a04a3aaa0e2bb4d
maxgarr <maxime.garron17@gmail.com>
2022-03-09 11:48:56 +0100

    Update dashboard com

--- src/view/user/gestion_commerciale/dashboard.php
+++ src/view/user/gestion_commerciale/dashboard.php
@@ -1,10 +1,20 @@
-<h1 class="text-center my-5">DASHBOARD Gestion commerciale</h1>
+<div class="container my-5">
+    <div class="row">
+        <div class="col-sm-8 offset-sm-2">
+            <div class="card">
+                <div class="card-body text-center" style="background-color: rgba(112, 114, 110, 0.5); box-shadow: 0px 10px 10px rgba(112, 114, 110, 0.8);">
+                    <h5 class="card-title" style="font-size: xx-large">Tableau de bord - Gestion Commerciale</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <div class="container">
     <div class="row">
         <div class="col-sm-3 offset-sm-3">
             <div class="card">
-                <div class="card-body text-center" style="background-color: rgba(112, 114, 110, 0.5); box-shadow: -10px 5px 10px rgba(112, 114, 110, 0.8);">
+                <div class="card-body text-center" style="background-color: rgba(112, 114, 110, 0.5); box-shadow: -10px 10px 10px rgba(112, 114, 110, 0.8);">
                     <h5 class="card-title">Liste des Clients</h5>
                     <a href="clients" class="btn btn-secondary" role="button">Afficher</a>
                 </div>
@@ -12,7 +22,7 @@
         </div>
         <div class="col-sm-3">
             <div class="card">
-                <div class="card-body text-center" style="background-color: rgba(112, 114, 110, 0.5); box-shadow: 10px 5px 10px rgba(112, 114, 110, 0.8);">
+                <div class="card-body text-center" style="background-color: rgba(112, 114, 110, 0.5); box-shadow: 10px 10px 10px rgba(112, 114, 110, 0.8);">
                     <h5 class="card-title">Liste des Contacts</h5>
                     <a href="contacts" class="btn btn-secondary" role="button">Afficher</a>
                 </div>
